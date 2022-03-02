3f5dc06fce37b5f323102db06194d242e7ae1521
Konrad Depowski <konrad6depowski@wp.pl>
2022-03-02 16:46:57 +0100

    main

---
+++ script.js
@@ -0,0 +1,34 @@
+const input = document.getElementById('create');
+const inptSumb = document.getElementById('submit');
+const btn = document.querySelector('.clear');
+const ul = document.querySelector('.taskList ul');
+
+
+const removeTask = (e) => {
+    let tar = e.target.parentNode;
+    tar.remove()
+}
+
+const addTask = () => {
+    if (input.value) {
+        const li = document.createElement('li');
+        ul.appendChild(li);
+        li.innerHTML = input.value + "<img class='remove' src='icons/remove.png'>";
+        input.value = '';
+    }
+
+    document.querySelectorAll('img.remove').forEach(item => {
+        item.addEventListener('click', removeTask)
+    })
+
+    document.querySelectorAll('li').forEach(it => {
+        btn.addEventListener('click', () => {
+            it.remove()
+        })
+
+    })
+
+}
+
+
+inptSumb.addEventListener('click', addTask)
